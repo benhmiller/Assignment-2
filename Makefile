c37c491edc31053f08a455cbc0a9c2596f7c8921
Benjamin Miller <benhmiller22@gmail.com>
2024-02-20 16:33:07 -0600

    Added Makefile with clean target to remove all .dat files

---
+++ Makefile
@@ -0,0 +1,5 @@
+clean:
+	rm items-*_Item.dat
+	rm items-*_Seller.dat
+	rm items-*_Bid.dat
+	rm items-*_Bidder.dat
--- runParser.sh
+++ runParser.sh
@@ -1,1 +1,5 @@
 python my_parser.py ebay_data/items-*.json
+cat items-*_Item.dat > Items.dat
+cat items-*_Seller.dat > Sellers.dat
+cat items-*_Bid.dat > Bids.dat
+cat items-*_Bidder.dat > Bidders.dat
