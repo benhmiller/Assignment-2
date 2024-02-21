a5bfd40ff0b8f6e3210786856f798ef8d79de9f9
Benjamin Miller <benhmiller22@gmail.com>
2024-02-20 23:50:52 -0600

    In runParser added option to store concatened .dat files in directory

--- runParser.sh
+++ runParser.sh
@@ -28,6 +28,10 @@ sqlite3 AuctionBase.db < load.txt
 
 # Clean directory of .dat files
 rm *.dat
+#rm items-*_Item.dat
+#rm items-*_Seller.dat
+#rm items-*_Bid.dat
+#rm items-*_Bidder.dat
 
 # Run SQL Query Files
 sqlite3 AuctionBase.db < query1.sql
